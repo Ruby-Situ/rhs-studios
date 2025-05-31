7d80b7b8839b660dfe853066c52a234fc5692673
Ruby-Situ <hsitu004@ucr.edu>
2025-05-30 23:21:13 -0700

    consol log

--- api/contact.js
+++ api/contact.js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
+  console.log("SMTP HOST:", process.env.SMTP_HOST);
 
   const { name, email, message } = req.body;
 
